0189f5613bd03c919b0a75f95235b9b45e5402fa
yjilakara <yjilakara@cambriasolutions.com>
2016-09-29 10:31:40 -0700

    Fixing build
    fixing build

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -1,15 +1,39 @@
-<<<<<<< HEAD
+
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using msrfp_3717.Data;
+using msrfp_3717.Models;
+using msrfp_3717.Services;
 using msrfp_3717.Models.ManageViewModels;
 using Newtonsoft.Json;
-
 namespace msrfp_3717.Controllers
 {
 
 
     public class SearchController : Controller
  {
+
+    //   private ApplicationDbContext appDbctx;
+    //     public SearchController(ApplicationDbContext ctx)
+    //     {
+    //       appDbctx = ctx;
+    //     }       
+
+      //  [HttpGet]
+        // public IActionResult SearchProvider()
+        // {
+        //     var prov = new Provider();
+        //     prov = appDbctx.Provider.First();
+        //     return View(prov);
+        // }
  [HttpGet]
       public IActionResult SearchProvider()
         {
@@ -91,38 +115,4 @@ namespace msrfp_3717.Controllers
         }
  }
  }
-=======
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.Extensions.Logging;
-using msrfp_3717.Data;
-using msrfp_3717.Models;
-using msrfp_3717.Services;
 
-namespace msrfp_3717.Controllers
-{
-    public class SearchController : Controller
-    {
-        private ApplicationDbContext appDbctx;
-        public SearchController(ApplicationDbContext ctx)
-        {
-          appDbctx = ctx;
-        }       
-
-        [HttpGet]
-        public IActionResult SearchProvider()
-        {
-            var prov = new Provider();
-            prov = appDbctx.Provider.First();
-            return View(prov);
-        }
-    }
-}
->>>>>>> origin/master
